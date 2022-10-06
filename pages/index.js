28c8c05028b5803715dc961c04f59d4580ee9cb4
Roger Pender <rdpender3@gmail.com>
2022-10-06 13:26:06 -0700

    refactoring code and getting api functional

--- pages/index.js
+++ pages/index.js
@@ -3,26 +3,14 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import Banner from '../components/banner';
 import Card from '../components/card';
-import coffeeShopsData from '../data/coffee-stores.json';
+import { fetchCoffeeShops } from '../lib/coffee-shops';
 
 export async function getStaticProps(context) {
 
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: ''
-    }
-  };
-  
-  const response = await fetch('https://api.foursquare.com/v3/places/search?query=coffee%20shop&near=Sacramento%2C%20CA&limit=6', options);
-
-  const data = await response.json();
-  
-    // .catch(err => console.error(err));
+  const coffeeShops = await fetchCoffeeShops();
 
   return {
-    props: {coffeeShops: data.results}, 
+    props: {coffeeShops}, 
   }
 
 }
@@ -50,7 +38,13 @@ export default function Home(props) {
           <h2 className={styles.heading2}>Sacramento Coffee Shops</h2>
           <div className={styles.cardLayout}>
             {props.coffeeShops.map(coffeeShop => {
-              return <Card key={coffeeShop.fsq_id} name={coffeeShop.name} imgUrl={coffeeShop.imgUrl || "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"} href={`/coffee-shop/${coffeeShop.id}`} className={styles.card} />
+              return <Card 
+                key={coffeeShop.fsq_id} 
+                name={coffeeShop.name} 
+                imgUrl={coffeeShop.imgUrl || "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"} 
+                href={`/coffee-shop/${coffeeShop.fsq_id}`} 
+                className={styles.card} 
+              />
             })}
           </div>
         </>}
