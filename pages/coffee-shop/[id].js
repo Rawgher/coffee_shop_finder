bb696380cb16aa5606179f25c6fc6501626be563
Roger Pender <rdpender3@gmail.com>
2022-10-06 11:12:46 -0700

    setting up static paths

--- pages/coffee-shop/[id].js
+++ pages/coffee-shop/[id].js
@@ -1,18 +1,52 @@
 import { useRouter } from "next/router";
 import Link from 'next/link';
+import Head from "next/head";
+import coffeeShopsData from '../../data/coffee-stores.json';
 
-const CoffeeShop = () => {
+export async function getStaticProps(staticProps) {
+    const params = staticProps.params;
+    return {
+      // Passed to the page component as props
+      props: { coffeeShop:  coffeeShopsData.find(coffeeShop => {
+        return coffeeShop.id.toString() === params.id;
+      }) },
+    }
+}
+
+export async function getStaticPaths() {
+    const paths = coffeeShopsData.map(coffeeShop => {
+        return {
+            params: {id: coffeeShop.id.toString()},
+        };
+    });
+    return {
+      paths,
+      fallback: true, 
+    };
+}
+
+const CoffeeShop = (props) => {
 
     const router = useRouter();
-    console.log(router)
+    console.log(router);
+
+    if (router.isFallback) {
+        return <div>Loading...</div>;
+    }
+
+    const {address, name, neighbourhood } = props.coffeeShop;
 
     return (
         <div>
+            <Head>
+                <title>{name}</title>
+            </Head>
             <Link href="/">
                 <a>Back to Home</a>
             </Link>
-
-            <h1>Single Coffee Shop Page {router.query.id}</h1>
+            <p>{address}</p>
+            <p>{name}</p>
+            <p>{neighbourhood}</p>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,17 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import Banner from '../components/banner';
 import Card from '../components/card';
-import coffeeShops from '../data/coffee-stores.json';
+import coffeeShopsData from '../data/coffee-stores.json';
 
-export default function Home() {
+export async function getStaticProps(context) {
+
+  return {
+    props: {coffeeShops: coffeeShopsData}, 
+  }
+
+}
+
+export default function Home(props) {
 
   const handleOnBannerBtnClick = () => {
     console.log('clicked');
@@ -23,11 +31,15 @@ export default function Home() {
         <div className={styles.heroImage}>
           <Image src='/static/hero-image.png' width={700} height={400} alt='hero image' />
         </div>
-        <div className={styles.cardLayout}>
-          {coffeeShops.map(coffeeShop => {
-            return <Card name={coffeeShop.name} imgUrl={coffeeShop.imgUrl} href={`/coffee-shop/${coffeeShop.id}`} className={styles.card} />
-          })}
-        </div>
+        {props.coffeeShops.length > 0 && 
+        <>
+          <h2 className={styles.heading2}>Sacramento Coffee Shops</h2>
+          <div className={styles.cardLayout}>
+            {props.coffeeShops.map(coffeeShop => {
+              return <Card name={coffeeShop.name} imgUrl={coffeeShop.imgUrl} href={`/coffee-shop/${coffeeShop.id}`} className={styles.card} key={coffeeShop.id} />
+            })}
+          </div>
+        </>}
       </main>
     </div>
   )
